e09b5f54fa2d00efbae9ffff3688f5a449019704
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-08 22:06:54 -0300

    Variaveis e começo de tippos de dados, aula02

---
+++ Python-Aula02/tiposDeDados.py
@@ -0,0 +1,42 @@
+# Tipos de Dados:
+# Inteiros(int) -> 1; 2; 3...
+# Decimal(float) -> 1.23; 345.8; 94.27...
+
+# Função que retorna o tipo de determinada variável: 
+# type(variavel)
+
+# Representação dos números em diferentes bases
+# Base binária (0 ou 1):
+# - 1010 = ??? (10 na base decimal)
+# Base octal: 
+# - 123 = ??? (83 em decimal)
+
+# Exercício 1:
+# dia = 1
+# horas = 3
+# minutos = 46
+# segundos = 40
+
+# diaHora = dia * 24
+# horasTotais = diaHora + horas
+# horasMin = horasTotais * 60
+# minTotais = horasMin + minutos
+# minSeg = minTotais * 60
+# segTotais = minSeg + segundos
+# print(segTotais)
+
+# Exercícios 2:
+# segundos = 87426
+# minTotais = segundos / 60
+# horaTotais = minTotais / 60
+# print(f"Horas: {horaTotais}, Minutos: {minTotais}, Segundos: {segundos}")
+
+# Exercício 3:
+#numero = 12345
+#produto = 1
+# 
+#for digito in str(numero):
+#    produto = produto * int(digito) 
+# - (também pode ser escrito assim -> produto *= int(digito)
+#print(produto)
+
---
+++ Python-Aula02/variaveis.py
@@ -0,0 +1,72 @@
+# Nomes usados para representar valores, só podem armazenar de cada vez,     devem começar com letras ou "_" e podem ter números.
+
+# Exitem também algumas palavras reservadas da linguagem, elas não podem ser utilizadas como nome  de variáveis.
+
+# Atribuir valores em variáveis (strings, int, float e etc):
+# a = "Olá" -> Atribuindo uma string a uma variavel;
+# b = 2 -> Atribuindo um int a uma variavel;
+# c = 1.5 -> Atribuindo um float a uma variavel;
+
+# Atribuindo valores e fazendo contas com variáveis:
+# a = 2 -> A recebe o valor de 2;
+# b = 3 -> B recebe o valor de 3;
+# print(a + b) -> Exibindo o resultado da soma entre as variáveis;
+
+# Diagramas de referência: mostra o estado de cada variável
+# A = True -> A = True
+# B = 5 * 3 -> B = 15
+# C = A -> C = True
+
+# Sequência de Execução: (a sequência é executada de cima para baixo)
+# A = 4
+# B = 5
+# C = A + B
+# A = 10
+# No final -> A = 10; B = 5; C = 9; 
+
+# Outro formato para escrever sequências: (a sequência é executada de cima para baixo, da esuqerda para a direita)
+# A = 10
+# A, B = 3 * A, A
+# No final -> A = 10; A = 3 * A; B = A
+
+# Exercício 1 :
+# nome = "Raul"
+# print(nome)
+
+# Exercício 2 :
+# a = 3
+# b = 5
+# print((2 * a) + (3 * b))
+
+# Exercício 3 :
+# a = 1
+# b = 2
+# c = 3
+# print(a + b + c)
+
+# Exercício 4 :
+# base = 2
+# altura = 4
+# print("A área do retângulo é:", base * altura)
+
+# Exercício 5 :
+# a = 1
+# b = -3
+# c = 2
+# delta = (b ** 2) - (4 * a * c)
+# rzDelta = delta ** 0.5
+# raiz1 = (-b + rzDelta) / (2 * a)
+# raiz2 = (-b - rzDelta) / (2 * a)
+# print("raiz 1 é igual a:", raiz1, "Raiz 2 é igual a:", raiz2)
+
+# Exercício 6 :
+# salario = 1000
+# aumento = (15 / 100) * salario
+# novoSalario = aumento + salario
+# print("Salario inicial:", salario, "Salario com aumento:", novoSalario)
+
+# Exercício 7 :
+# salario = 1000
+# decrescimo = (15 / 100) * salario
+# novoSalario = salario - descrescimo
+
