14c2ccca637f1c15c0871d3fbf046a5fed8cefca
RaulLaguna2310 <raullaguna2310@gmail.com>
2025-05-13 00:04:11 -0300

    Finalização da Aula05 e inicio da Aula06

---
+++ Python-Aula05/FStringsComposicao.py
@@ -0,0 +1,12 @@
+# Composição de Strings:
+# - Uma string é composta por letras, números, caracteres especiais ou espaço em branco;
+# - Sempre escrito entre aspas "";
+# a = "Hoje tem aula!"
+
+# F-Strings:
+# nome1 = 'Maria'
+# nome2 = 'João'
+# ano = 2000
+# print(f"{nome2} e {nome1} nasceram em {ano}")
+# Utilizamos variáveis dentro de strings, editar sua exibição e fazer operações matemáticas
+# print(f"{int(x)}") -> Podemos também chamar funções dentro delas
---
+++ Python-Aula06/tratamentoDeStrings.py
@@ -0,0 +1,2 @@
+# Tratamento de Strings:
+# len(a) -> é uma expressão que retorna o número de caracteres de uma string;
